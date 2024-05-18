022b619055e6baaa02344e36a5e152653f4348a7
Xanders Wachira <muhindia66@gmail.com>
2024-05-18 02:41:21 -0700

    Textme

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+log_file_path = ARGV[0]
+pattern =/\[from:(?<sender>[\w+: ] \[to:(?<receiver>[\w+:]+)\] \[flags:(?<flags>[-\d:]+)\]/
+File.open(log_file_path, "r").each_line do |line|
+  match = line.match(pattern)
+  if match
+    sender = match[:sender].gsub(/\s+/, "")
+    receiver = match[:receiver].gsub(/\s+/, "")
+    flags = match[:flags]
+    puts "#{sender},#{receiver},#{flags}"
+  end
+end
