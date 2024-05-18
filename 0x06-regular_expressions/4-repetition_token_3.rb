4b49cff0a1899ac83613137d17d6b87079ba6f8a
Xanders Wachira <muhindia66@gmail.com>
2024-05-18 02:03:08 -0700

    t repetion 0 or more times.

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,6 +1,10 @@
 #!/usr/bin/env ruby
-input = ARGV[0]
-pattern = /hbt*n./
+if ARGV.length != 1
+  puts "Usage: #{$0} <string>"
+  exit 1
+end
+input_sting = ARGV[0]
+pattern = /^hbt*n$/
 matches = input.scan(pattern)
 matches.each { |match| print match }
 puts
